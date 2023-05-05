0afbb2e7037e056fe9c4b6c4e224c8fc2f82d7d2
Ignacio Perez <ignacio.perez.jobs@gmail.com>
2023-05-05 19:34:44 -0300

    delete implicit return,use of env, add useMemo in Card-components

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import {ImageUrl} from '../../services/urls';
+import React, {useMemo} from 'react';
 import {cryptoData} from '../../model/crypto.model';
 import ArrowUp from 'react-native-vector-icons/Feather';
 import ArrowDown from 'react-native-vector-icons/Feather';
@@ -15,6 +14,7 @@ import {
   TextLogo,
   TextVarian,
 } from './styles';
+import {IMAGE_URL} from '@env';
 
 const Card = ({id, name, symbol, market_data}: cryptoData) => {
   const formatter = new Intl.NumberFormat('en-US', {
@@ -23,15 +23,18 @@ const Card = ({id, name, symbol, market_data}: cryptoData) => {
     minimumFractionDigits: 0,
   });
 
-  const roundNumber = market_data.percent_change_btc_last_24_hours.toFixed(2);
-  const roundNegative = Number(roundNumber) < 0;
+  const memoizedRoundNumbers = useMemo(() => {
+    const roundNumber = market_data.percent_change_btc_last_24_hours.toFixed(2);
+    const roundNegative = Number(roundNumber) < 0;
+    return {roundNumber, roundNegative};
+  }, [market_data]);
 
   return (
     <CryptoCard>
       <Logo>
         <CryptoImage
           source={{
-            uri: `${ImageUrl}${id}/64.png?v=`,
+            uri: `${IMAGE_URL}${id}/64.png?v=`,
           }}
         />
         <TextLogo>
@@ -44,14 +47,16 @@ const Card = ({id, name, symbol, market_data}: cryptoData) => {
           <FirstText>{formatter.format(market_data.price_usd)}</FirstText>
         </FirstData>
         <SecondData>
-          <TextVarian roundNegative={roundNegative}>
-            {roundNegative ? (
+          <TextVarian roundNegative={memoizedRoundNumbers.roundNegative}>
+            {memoizedRoundNumbers.roundNegative ? (
               <ArrowDown name="arrow-down-left" size={20} />
             ) : (
               <ArrowUp name="arrow-up-right" size={20} />
             )}
           </TextVarian>
-          <TextVarian roundNegative={roundNegative}>{roundNumber}%</TextVarian>
+          <TextVarian roundNegative={memoizedRoundNumbers.roundNegative}>
+            {memoizedRoundNumbers.roundNumber}%
+          </TextVarian>
         </SecondData>
       </DataCrypto>
     </CryptoCard>
--- src/services/urls.ts
+++
@@ -1,5 +0,0 @@
-export const ImageUrl = 'https://asset-images.messari.io/images/';
-
-export const baseUrl = 'https://data.messari.io/api/v2';
-
-export const searchUrl = 'https://data.messari.io/api/v1/assets';
